7889646719147048e7c06d58fa31a0ce0f8c689e
xxy <xxy@951507141>
2022-04-28 18:02:12 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author qingwen
+ * @creat 2022-04-2022/4/28-17:56
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
